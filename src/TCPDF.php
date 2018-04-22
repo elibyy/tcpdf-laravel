c6349817d285e598a1e5e05b7b0843566bb3bdb5
elibyy <elibyy@gmail.com>
2018-04-22 22:39:31 +0300

    Added fpdi support Added options to use original `Header()`/`Footer()`

--- src/TCPDF.php
+++ src/TCPDF.php
@@ -6,21 +6,23 @@ use Config;
 
 class TCPDF
 {
-	protected static $format;
-	
-	protected $app;
-	/** @var  TCPDFHelper */
-	protected $tcpdf;
+    protected static $format;
 
-	public function __construct($app)
-	{
-		$this->app = $app;
-		$this->reset();
-	}
+    protected $app;
+    /** @var  TCPDFHelper */
+    protected $tcpdf;
+
+    public function __construct($app)
+    {
+        $this->app = $app;
+        $this->reset();
+    }
 
 	public function reset()
 	{
-		$this->tcpdf = new TCPDFHelper(
+		$class = config('tcpdf.use_fpdi') ? FpdiTCPDFHelper::class : TCPDFHelper::class;
+
+        $this->tcpdf = new $class(
 			Config::get('tcpdf.page_orientation', 'P'),
 			Config::get('tcpdf.page_units', 'mm'),
 			static::$format ? static::$format : Config::get('tcpdf.page_format', 'A4'),
@@ -29,26 +31,26 @@ class TCPDF
 		);
 	}
 
-	public static function changeFormat($format)
-	{
-		static::$format = $format;
-	}
+    public static function changeFormat($format)
+    {
+        static::$format = $format;
+    }
 
-	public function __call($method, $args)
-	{
-		if (method_exists($this->tcpdf, $method)) {
-			return call_user_func_array([$this->tcpdf, $method], $args);
-		}
-		throw new \RuntimeException(sprintf('the method %s does not exists in TCPDF', $method));
-	}
+    public function __call($method, $args)
+    {
+        if (method_exists($this->tcpdf, $method)) {
+            return call_user_func_array([$this->tcpdf, $method], $args);
+        }
+        throw new \RuntimeException(sprintf('the method %s does not exists in TCPDF', $method));
+    }
 
-	public function setHeaderCallback($headerCallback)
-	{
-		$this->tcpdf->setHeaderCallback($headerCallback);
-	}
+    public function setHeaderCallback($headerCallback)
+    {
+        $this->tcpdf->setHeaderCallback($headerCallback);
+    }
 
-	public function setFooterCallback($footerCallback)
-	{
-		$this->tcpdf->setFooterCallback($footerCallback);
-	}
+    public function setFooterCallback($footerCallback)
+    {
+        $this->tcpdf->setFooterCallback($footerCallback);
+    }
 }
