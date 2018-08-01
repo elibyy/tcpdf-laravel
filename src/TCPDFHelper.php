cec642e1479a5ee96069f98bf0ecd62cbf0aa5b2
Stelian-Andrei Adumitroaei <andrei.adumitroaei@ultragrup.ro>
2018-08-01 17:26:23 +0300

    Add bookmark sorting before TOC creation

--- src/TCPDFHelper.php
+++ src/TCPDFHelper.php
@@ -44,4 +44,19 @@ class TCPDFHelper extends \TCPDF
         $this->footerCallback = $callback;
     }
 
+    public function addTOC($page = '', $numbersfont = '', $filler = '.', $toc_name = 'TOC', $style = '', $color = array(0, 0, 0))
+    {
+        // sort bookmarks before generating the TOC
+        parent::sortBookmarks();
+
+        parent::addTOC($page, $numbersfont, $filler, $toc_name, $style, $color);
+    }
+
+    public function addHTMLTOC($page = '', $toc_name = 'TOC', $templates = array(), $correct_align = true, $style = '', $color = array(0, 0, 0))
+    {
+        // sort bookmarks before generating the TOC
+        parent::sortBookmarks();
+
+        parent::addHTMLTOC($page, $toc_name, $templates, $correct_align, $style, $color);
+    }
 }
