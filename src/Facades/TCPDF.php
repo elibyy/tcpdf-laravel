ee9d1652d4bf7b869daf456feaf1aa35cf461807
Ahmet Imamoglu <ahmet363@gmail.com>
2023-11-19 17:26:43 +0300

    Added auto-completion for PhpStorm.

--- src/Facades/TCPDF.php
+++ src/Facades/TCPDF.php
@@ -3,6 +3,9 @@ namespace Elibyy\TCPDF\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @mixin \TCPDF
+ */
 class TCPDF extends Facade
 {
 	protected static function getFacadeAccessor(){return 'tcpdf';}
